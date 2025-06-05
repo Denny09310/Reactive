423118fdf1fa0210e4a1dc078a8675860bec1fa4
Denny09310 <k.denny2000@gmail.com>
2025-06-05 14:44:36 +0200

    chore: removed debounce as it's not used for now

--- src/Reactive.Core/Utils/Scheduler.cs
+++ src/Reactive.Core/Utils/Scheduler.cs
@@ -3,7 +3,6 @@
 public static class Scheduler
 {
     private static readonly HashSet<Action> _effects = [];
-    private static readonly Dictionary<string, CancellationTokenSource> _tokens = new();
     private static bool _batching = false;
 
     public static void Batch(Action action)
@@ -18,27 +17,6 @@ public static class Scheduler
         }
 
         _effects.Clear();
-    }
-
-    public static void Debounce(string key, TimeSpan delay, Action action)
-    {
-        if (_tokens.TryGetValue(key, out var existing))
-        {
-            existing.Cancel();
-            existing.Dispose();
-        }
-
-        var cts = new CancellationTokenSource();
-        _tokens[key] = cts;
-
-        Task.Delay(delay, cts.Token).ContinueWith(task =>
-        {
-            if (!task.IsCanceled)
-            {
-                action();
-                _tokens.Remove(key);
-            }
-        }, TaskScheduler.Default);
     }
 
     public static void Schedule(Action effect)
